e6dd1039920c1a22d2001bf781830dfc78b8f17f
Lucas Nascimento <lucas.silva2416@etec.sp.gov.br>
2018-12-27 00:50:18 -0200

    first java-io

---
+++ java-io/src/br/com/lucas/java/io/teste/TesteLeitura.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lucas.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author User
+ */
+public class TesteLeitura {
+    public static void main(String[] args) throws IOException {
+        //Fluxo de entrada com um arquivo
+        
+        FileInputStream file = new FileInputStream("arquivo para texto.txt");
+        InputStreamReader isr = new InputStreamReader(file);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String linha = br.readLine();
+        
+        System.out.println(linha);
+    }
+}
