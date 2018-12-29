ad007e0c71ec36b2703699fb9ee1231f84228185
Lucas Nascimento <lucas.silva2416@etec.sp.gov.br>
2018-12-29 16:32:13 -0200

    Correções

---
+++ java-io/src/br/com/lucas/java/io/teste/TesteCopiarArquivo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lucas.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author User
+ */
+public class TesteCopiarArquivo {
+    public static void main(String[] args) throws IOException {
+        //Fluxo de entrada com um arquivo
+        
+        // Padrão decorator
+       /* FileInputStream file = new FileInputStream("arquivo para texto.txt");
+        InputStreamReader isr = new InputStreamReader(file);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String linha = br.readLine();
+        
+        while(linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+        
+        */
+    }
+}
--- java-io/src/br/com/lucas/java/io/teste/TesteEscrita.java
+++ java-io/src/br/com/lucas/java/io/teste/TesteEscrita.java
@@ -9,6 +9,7 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
@@ -18,19 +19,19 @@ import java.io.Writer;
  */
 public class TesteEscrita {
     public static void main(String[] args) throws IOException {
-        //Fluxo de entrada com um arquivo
-        
-        // Padrão decorator
-        OutputStream fos = new FileOutputStream("arquivo2.txt");
+       OutputStream fos = new FileOutputStream("lorem2.txt");
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
-        
-        bw.write("Lucas Nascimento da silva, Bom dia!!!!");
-        //bw.newLine();
+
+        bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
         bw.newLine();
-        bw.write("Testando FileOutputStream");
-        
+        bw.write("tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam");
+
         bw.close();
+       
+       
+        
+       
         
         
     }
