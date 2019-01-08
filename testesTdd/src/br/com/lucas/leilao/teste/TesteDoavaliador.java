d3ffceb46b0a798a570c8749b4d45b25c4d28073
Lucas <lucas.nascimento@logicsp.com.br>
2019-01-08 17:39:50 -0200

    Iniciando TDD

--- testesTdd/src/br/com/lucas/leilao/teste/TesteDoavaliador.java
+++ testesTdd/src/br/com/lucas/leilao/teste/TesteDoavaliador.java
@@ -9,13 +9,20 @@ import br.com.lucas.leilao.Lance;
 import br.com.lucas.leilao.Leilao;
 import br.com.lucas.leilao.Usuario;
 import br.com.lucas.leilao.servico.Avaliador;
+import junit.framework.Assert;
+import org.junit.Test;
 
 /**
  *
  * @author lucas
  */
 public class TesteDoavaliador {
-    public static void main(String[] args) {
+    // para que o jUnit entenda o método de teste temos que mudar a assinatura
+    // o método não pode ser estatico e nem receber argumentos
+    // Também deve ser anotado com @Test
+    @Test
+    public void main() {
+        // Parte 1: Cenário
         Usuario joao = new Usuario("Huguinho");
         Usuario luizinho = new Usuario("Luizinho");
         Usuario zezinho = new Usuario("zezinho");
@@ -26,10 +33,17 @@ public class TesteDoavaliador {
         leilao.propoe(new Lance(luizinho, 300.0));
         leilao.propoe(new Lance(zezinho, 400.0));
         
+        // parte 2: ação
         Avaliador leiloeiro = new Avaliador();
         leiloeiro.avalia(leilao);
         
-        System.out.println(leiloeiro.getMaiorLance());
-        System.out.println(leiloeiro.getMenorDeTodos());
+        // parte 3: Validação
+        double maiorEsperado = 400;
+        double menorEsperado = 250;
+        
+     //   System.out.println(maiorEsperado == leiloeiro.getMaiorLance());
+        Assert.assertEquals(menorEsperado, maiorEsperado, 0.00001);
+       // System.out.println(menorEsperado == leiloeiro.getMenorDeTodos());
+        Assert.assertEquals(menorEsperado, maiorEsperado, 0.00001);
     }
 }
